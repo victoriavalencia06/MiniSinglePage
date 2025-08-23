d1b512f31a45885686a5263ca9e68d55fdf944c0
Victoria Reyes <victoriavalencia2006@outlook.com>
2025-08-22 22:21:13 -0600

    Instalacion y configuracion de firebase

---
+++ minisingle-citas/services/dataServices.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLvo8luNDP256K4y7FmScS5cj30yyQzzE",
+  authDomain: "minisingle-citas.firebaseapp.com",
+  projectId: "minisingle-citas",
+  storageBucket: "minisingle-citas.firebasestorage.app",
+  messagingSenderId: "156612784484",
+  appId: "1:156612784484:web:1fc92ebf219c8314fa8a71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
